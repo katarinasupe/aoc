5bcd6980e5afd4100803358857ff49ac7e09d824
katarinasupe <supe.katarina@gmail.com>
2021-12-01 22:29:36 +0100

    Add first solution

---
+++ 2021/1/1.py
@@ -0,0 +1,20 @@
+file = "1.txt"
+increased = 0
+with open(file) as f:
+    lines = f.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+    previous = lines[0]
+    for i in range(1, len(lines)):
+        if lines[i] > previous:
+            increased += 1
+        previous = lines[i]
+    print(increased)
+
+    previous_sum = lines[0] + lines[1] + lines[2]
+    increased_three = 0
+    for j in range(1, len(lines) - 2):
+        sum_next = lines[j] + lines[j+1] + lines[j+2]
+        if sum_next > previous_sum:
+            increased_three += 1
+        previous_sum = sum_next
+    print(increased_three)
